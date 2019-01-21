e6338df8427275d26f4ecf49edb975d0c3e513f9
JonaSilveira <jonassilvasilveira@souunisuam.com.br>
2019-01-20 22:06:25 -0200

    Commit for android

--- Faculdade/app/src/main/java/com/example/jonas/faculdade/AvisoActivity.java
+++ Faculdade/app/src/main/java/com/example/jonas/faculdade/AvisoActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 public class AvisoActivity extends AppCompatActivity {
 
@@ -33,8 +34,35 @@ public class AvisoActivity extends AppCompatActivity {
 
     }
 
+    public void ClickLimpar(View v){
+        nometxt.setText("");
+        salva.setText("");
+    }
+
+    public void ClicSalvar (View v) {
+        Salvar();
+    }
+
     private String GetRoot() {
         File root = android.os.Environment.getExternalStorageDirectory();
         return ((File) root).toString();
+    }
+
+    private void Salvar(){
+        String lerNomeTxt;
+        File arquivo;
+        byte[] dadod;
+        try{
+            //pega o nome do arquivo a ser gravado
+            lerNomeTxt = nometxt.getText().toString();
+            arquivo = new File(Environment.getExternalStorageDirectory(), lerNomeTxt);
+            FileOutputStream out;
+            out = new FileOutputStream(arquivo);
+            out.flush();
+            out.close();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
